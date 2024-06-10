e04d06ba674a2f9c56eba9a1fb241f160f9f477e
pisit-wongthawin <pisitwongthawin1234@gmail.com>
2024-06-11 00:41:21 +0700

    FIXED DRY

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -14,12 +14,8 @@ const handleLogout = async (req, res) => {
 
     // Is refreshToken in db?
     const foundUser = usersDB.users.find(person => person.refreshToken === refreshToken);
-    if (!foundUser) {
-        res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
-        return res.sendStatus(204);
-    }
-
-    // Delete refreshToken in db
+    if (foundUser) {
+        // Delete refreshToken in db
     const otherUsers = usersDB.users.filter(person => person.refreshToken !== foundUser.refreshToken);
     const currentUser = { ...foundUser, refreshToken: '' };
     usersDB.setUsers([...otherUsers, currentUser]);
@@ -27,6 +23,7 @@ const handleLogout = async (req, res) => {
         path.join(__dirname, '..', 'model', 'users.json'),
         JSON.stringify(usersDB.users)
     );
+    }
 
     res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
     res.sendStatus(204);
